2c24011fc9feb3b04b0a4753cfeaeb716eaa1bff
azadsadr <azad.sadr.h@gmail.com>
2022-09-30 12:23:59 +0200

    modify Dataset class and cnn architecture

--- cnn.py
+++ cnn.py
@@ -3,9 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ConvNN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(ConvNN, self).__init__()
+        super(CNN, self).__init__()
+
+        '''
+        torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, ... )
+            in_channels (int)                       : Number of channels in the input image
+            out_channels (int)                      : Number of channels produced by the convolution
+            kernel_size (int or tuple)              : Size of the convolving kernel
+            stride (int or tuple, optional)         : Stride of the convolution. Default: 1
+            padding (int, tuple or str, optional)   : Padding added to all four sides of the input. Default: 0
+
+        torch.nn.MaxPool2d(kernel_size, stride=None, padding=0, ... )
+            kernel_size : the size of the window to take a max over
+            stride      : the stride of the window. Default value is kernel_size
+            padding     : implicit zero padding to be added on both sides
+
+        torch.nn.Dropout2d(p=0.5, inplace=False)
+            p (float, optional) : probability of an element to be zero-ed.
+
+        torch.nn.Linear(in_features, out_features, bias=True, ... )
+            in_features     : size of each input sample
+            out_features    : size of each output sample
+            bias            : If set to False, the layer will not learn an additive bias. Default: True
+        '''
+
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3,3))
         self.pool1 = nn.MaxPool2d(kernel_size=(2,2))
 
@@ -18,7 +41,7 @@ class ConvNN(nn.Module):
         self.fc1 = nn.Linear(30*3*3, 270)
         self.fc2 = nn.Linear(270, 24)
 
-        self.softmax = nn.LogSoftmax(dim=1)
+        #self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
         output = self.conv1(x)
@@ -33,28 +56,38 @@ class ConvNN(nn.Module):
         output = F.relu(output)
         output = self.dropout1(output)
 
+        # When we don’t know how many rows or columns you want, 
+        # PyTorch can automatically set a value for you when you pass in -1. 
+        # In our case, we know our columns will be 30 * 3 * 3, but we don’t know how many rows we want.
         output = output.view(-1, 30*3*3)
+        
         output = F.relu(self.fc1(output))
         output = F.relu(self.fc2(output))
-        output = self.softmax(output)
+        #output = self.softmax(output)
         return output
+    
 
+def train(train_loader, valid_loader, model, num_epochs, learning_rate, device):
 
-def train(loader, model, num_epochs, learning_rate, device):
+    train_losses = []
+    valid_losses = []
     
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    n_total_steps = len(train_loader)
 
-    n_total_steps = len(loader)
-    model.train()
+    # An epoch is one pass over the entire train set
     for epoch in range(num_epochs): # no. of full passes (loop) over the data
-        #running_loss = 0
-        #print(f'epoch: {epoch+1}')
 
-        for batch_idx, (images, labels) in enumerate(loader):
+        train_loss = 0.0
+        valid_loss = 0.0
+
+        # training the model
+        model.train()
+        for batch_idx, (images, labels) in enumerate(train_loader):
 
             # get data as a list of [images, labels]
-            # train_loader is a batch of featuresets and labels
+            # loader is a batch of featuresets and labels
             # batch_idx : index of the batch
             # images    : one batch of features
             # labels    : one batch of targets
@@ -68,30 +101,58 @@ def train(loader, model, num_epochs, learning_rate, device):
             # we did it inside CNN class
             #images = print("images.shape:", images.shape)
             
-            # forward propagation
+            # forward propagation (pass the inputs into neural network)
             outputs = model(images) # (batch_size x num_classes)
-            
-            #output = net(X.view(-1, 28*28)) # pass in reshaped batch
 
+            # passing our ouputs (a flattened layer of logits) from the network into a log softmax function and negative log likelihood.
+            # All this allows us to get the prediction error (loss) of our network.
+            # Note that outputs is our input (the predicted class), and labels is our target (the correct class).
             loss = criterion(outputs, labels)
             #loss = F.nll_loss(output, y)    # calc and grab loss value
 
-            # zero previous gradients. you will do this likely every step
+            # zero previous parameter gradients. you will do this likely every step
+            # It’s a crucial step to zero out the gradients or else all the gradients from multiple passes will accumulated
             optimizer.zero_grad()
-            # back-propagation
+
+            # performing back-propagation by computing the gradient based on the loss.
             loss.backward()
+
             # gradient descent or adam step (optimize weights)
+            # After computing gradient using backward(), 
+            # we can call the optimizer step function, 
+            # which iterates over all the parameters and update their values.
             optimizer.step()
 
-            #running_loss += loss.item()
+            # item() extracts the loss’s value as a Python float. 
+            # We then add it to our train_loss (which is zero at the start of every iteration)
+            train_loss += loss.item() * images.size(0)
 
             if (batch_idx+1) % 200 == 0:
                 print(f'epoch [{epoch+1}/{num_epochs}], batch [{batch_idx+1}/{n_total_steps}], loss = {loss.item():.4f}')
+        
+        #print("==============================================================")
+        # validate the model
+        model.eval()
+        for batch_idx, (images, labels) in enumerate(valid_loader):
+            images = images.to(device=device)
+            labels = labels.to(device=device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            valid_loss += loss.item() * images.size(0)
 
-        #print("loss =", loss.item()) # print loss. we hope loss (a measure of wrong-ness) declines!
-        print("==============================================================")
+        # calculate average losses
+        train_loss = train_loss / len(train_loader.sampler)
+        valid_loss = valid_loss / len(valid_loader.sampler)
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
 
-    return model
+        # print-training/validation-statistics
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, train_loss, valid_loss))
+
+
+    return model, train_losses, valid_losses
+
 
 
 def get_accuracy(loader, model, device):
@@ -101,19 +162,21 @@ def get_accuracy(loader, model, device):
 
     model.eval()
     # Test the model. we don't need to compute gradients (for memory efficiency)
+    # torch.no_grad is used when we don’t require PyTorch to run its autograd engine, 
+    # in other words, calculate the gradients of our input. Since we’re only calculating the accuracy of our network. 
+    # This will help reduce memory usage and speed up computation.
     with torch.no_grad():
         for index, (images, labels) in enumerate(loader):
             images = images.to(device=device)
             labels = labels.to(device=device)
-            # images are in correct shape
-            # no need to flatten MNIST images like normal neural network
-            # we did it inside CNN class
-            
             outputs = model(images)
-            
             _, predictions = torch.max(outputs, 1) # max returns (value, index)
-            #_, predictions = outputs.max(1)
+            
+            # this is a boolean expression. 
+            # We can sum the amount of times we get the right prediction, 
+            # and then grab the numeric value using item()
             num_correct += (predictions == labels).sum().item()
+
             num_total += labels.shape[0]
         
         print(f'Correct: [{num_correct} / {num_total}] with accuracy {float(num_correct)/float(num_total)*100:.2f} %')
--- preprocessing.py
+++ preprocessing.py
@@ -1,43 +1,84 @@
 import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import Dataset 
 
 
-class SignDataset(Dataset):
+class ASLDataset(Dataset):
 
-    def __init__(self, csv_path, transform=None):
-        
-        xy = np.loadtxt(csv_path, delimiter=",", dtype=np.float32, skiprows=1)
-        self.images = xy[:, 1:]
-        self.labels = xy[:, 0]
+    def __init__(self, csv_file, transform=None, target_transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            transform and target_trandform (callable, optional): Optional transform to be applied on a data.
+
+        Note: ToTensor() Converts a PIL Image or numpy.ndarray (H x W x C) in the range [0, 255] to a 
+        torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0] 
+        if the PIL Image belongs to one of the modes (L, LA, P, I, F, RGB, YCbCr, RGBA, CMYK, 1) or 
+        if the numpy.ndarray has dtype = np.uint8
+
+        """
+
+        # read as pandas dataframe and convert it to numpy -------------------------------
+        data = pd.read_csv(csv_file) # pandas dataframe
+        #self.images = data.iloc[:, 1:].to_numpy(dtype = 'float32')
+        self.images = data.iloc[:, 1:].to_numpy(dtype = 'uint8')
+        self.labels = data['label'].values
+
+        # read as numpy array
+        #data = np.loadtxt(csv_file, delimiter=",", dtype=np.float32, skiprows=1)
+        #self.images = data[:, 1:]
+        #self.labels = data[:, 0]
 
         change = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
         for i in range(self.images.shape[0]):
             if self.labels[i] in change:
                 self.labels[i] -= 1
+
+        # convert to tensor --------------------------------------------------------------
+        #self.images = torch.from_numpy(images)
+        #self.labels = torch.from_numpy(labels)
+        #self.labels = torch.from_numpy(labels).type(torch.LongTensor)
         
         self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.images)
 
-    def __getitem__(self, index):
-        
-        if torch.is_tensor(index):
-            index = index.tolist()
-        
-        x = self.images[index, :]
-        x = x.reshape(1, 28, 28)
-        y = self.labels[index]
-        y = np.array(y)
-        sample = x, y
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image = self.images[idx, :]
+        image = image.reshape(28, 28, 1)    # ToTensor() Converts (H x W x C) to the shape of (C x H x W)
+        label = self.labels[idx]
+
+        #label = np.array(self.labels[idx]).astype('long')
+        #image = torch.from_numpy(image)
+        #label = torch.from_numpy(label).type(torch.LongTensor)
 
         if self.transform:
-            sample = self.transform(sample)
+            image = self.transform(image)
         
-        return sample
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
+
+
+#---------------------------------------------------------------------
 
 class ToTensor:
     def __call__(self, sample):
         images, labels = sample
         return torch.from_numpy(images), torch.from_numpy(labels).type(torch.LongTensor)
+
+
+class Normalize:
+    def __call__(self, sample):
+        images, labels = sample
+        mean = torch.mean(images)
+        std = torch.std(images)
+        images = (images - mean) / std
+        return images, labels
